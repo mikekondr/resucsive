83d58d84542e194ac49a42dfe18fc2d628cb0ab1
Михайло Кондратенко <mike.kondr@gmail.com>
2023-06-10 12:02:45 +0300

    example

---
+++ example/frmExample.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace example
+{
+    partial class frmExample
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmExample";
+            this.Text = "Example";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmExample_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ example/frmExample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace example
+{
+    public partial class frmExample : Form
+    {
+        private bool flag = false;
+
+        public frmExample()
+        {
+            InitializeComponent();
+        }
+
+        private void MyDraw(Graphics g, int N, int x, int y)
+        {
+            flag = true;
+            timer1.Enabled = true;
+            while (flag)
+                Application.DoEvents();
+            timer1.Enabled = false;
+
+            if (N == 0)
+                return;
+            else
+            {
+                g.DrawRectangle(new Pen(Brushes.Blue, 2),
+                    0, 0, x, y);
+                x += 50;
+                y += 50;
+                N--;
+                MyDraw(g, N, x, y);
+            }
+        }
+
+        private void frmExample_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            MyDraw(g, 7, 50, 50);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            flag = false;
+        }
+    }
+}
