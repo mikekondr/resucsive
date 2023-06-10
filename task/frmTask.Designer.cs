2e6bc0c8b2c31bb9c7c46bb53bbf12b678fbe62b
Михайло Кондратенко <mike.kondr@gmail.com>
2023-06-10 14:28:47 +0300

    task_1

---
+++ task/frmTask.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace task
+{
+    partial class frmTask
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTask7 = new System.Windows.Forms.Button();
+            this.btnTask12 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 524);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnTask7
+            // 
+            this.btnTask7.Location = new System.Drawing.Point(9, 10);
+            this.btnTask7.Name = "btnTask7";
+            this.btnTask7.Size = new System.Drawing.Size(75, 23);
+            this.btnTask7.TabIndex = 1;
+            this.btnTask7.Text = "Варіант 7";
+            this.btnTask7.UseVisualStyleBackColor = true;
+            this.btnTask7.Click += new System.EventHandler(this.btnTask7_Click);
+            // 
+            // btnTask12
+            // 
+            this.btnTask12.Location = new System.Drawing.Point(9, 39);
+            this.btnTask12.Name = "btnTask12";
+            this.btnTask12.Size = new System.Drawing.Size(75, 23);
+            this.btnTask12.TabIndex = 2;
+            this.btnTask12.Text = "Варіант 12";
+            this.btnTask12.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnTask7);
+            this.panel2.Controls.Add(this.btnTask12);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(518, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(93, 524);
+            this.panel2.TabIndex = 3;
+            // 
+            // frmTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 524);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmTask";
+            this.Text = "Рекурсивне малювання";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTask7;
+        private System.Windows.Forms.Button btnTask12;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ task/frmTask.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace task
+{
+    public partial class frmTask : Form
+    {
+        private bool timerFlag = false;
+        private bool isRunning = false;
+
+        public frmTask()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTask7_Click(object sender, System.EventArgs e)
+        {
+            if (isRunning)
+            {
+                MessageBox.Show("Зачекайте завершення поточного виводу малюнка...", "Помилка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(BackColor);
+            int count = 0;
+
+            while (true)
+            {
+                string str = Interaction.InputBox("Введіть кількість елементів:", "Рекурсивне малювання", "8");
+                if (str == "")
+                    return;
+
+                if (!Int32.TryParse(str, out count) || count <= 0)
+                    MessageBox.Show("Введіть дійсне, додатнє число!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    break;
+            }
+
+            // width / 2 = X center
+            // height / 2 = Y center
+            // min(X, Y) / 2 - R guide radius
+            int X = panel1.Width / 2;
+            int Y = panel1.Height / 2;
+            int R = Math.Min(X, Y) / 2;
+            //  max radius = l/2 = (2*(pi/count)*R)/2
+            //  step radius = max radius /count
+            int dr = (int)(2 * (Math.PI/(double)count) * R / 2.0) / count;
+
+            if (dr == 0)
+            {
+                MessageBox.Show("За такої кількості елементів, найменше коло стає занадто малим (r < 1).\n" +
+                    "Вкажіть меншу кількість елементів.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+
+            timer1.Interval = 3000 / count;
+            isRunning = true;
+            draw7(g, 1, count, X, Y, R, dr);
+        }
+
+        private void draw7(Graphics g, int N, int count, int X, int Y, int R, int dr)
+        {
+            pause();
+
+            // step = 2pi/count
+            int x = X + (int)(R * Math.Cos(2 * Math.PI / count * N));
+            int y = Y + (int)(R * Math.Sin(2 * Math.PI / count * N));
+            int r = (count - N + 1) * dr;
+
+            g.DrawEllipse(new Pen(Brushes.Blue), new Rectangle(x-r, y-r, r*2, r*2));
+            if (N == count)
+            {
+                isRunning = false;
+                return;
+            }
+            else
+            {
+                N++;
+                draw7(g, N, count, X, Y, R, dr);
+            }
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            timerFlag = false;
+        }
+
+        private void pause()
+        {
+            timerFlag = true;
+            timer1.Enabled = true;
+            while (timerFlag)
+                Application.DoEvents();
+            timer1.Enabled = false;
+        }
+    }
+}
